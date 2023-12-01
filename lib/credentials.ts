5249eca8193fc2c822e77249529e5e7e81d42207
zidan kharisma a <zidan.kharisma@ui.ac.id>
2023-12-01 07:34:22 +0700

    c

--- lib/credentials.ts
+++ lib/credentials.ts
@@ -1,2 +1,2 @@
-export const MAPBOX_TOKEN = process.env.NEXT_PUBLIC_MAPBOX_TOKEN ?? "";
-export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+export const MAPBOX_TOKEN = process.env.NEXT_PUBLIC_MAPBOX_TOKEN ?? "pk.eyJ1IjoiemlkYW5raGFyaXNtYSIsImEiOiJjbG8zemN5cWswMnBpMmpudXEyYWZta2M3In0.SnGCAMJEYUBkaGMwZCQMpA";
+export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL ?? "localhost:8080";
