92c8a78caee59a77c3b7b8e0e53855eedb315204
Taufik Pragusga <taufik@pragusga.com>
2023-12-06 14:55:30 +0700

    p

--- components/Charts/channel.tsx
+++ components/Charts/channel.tsx
@@ -1,22 +1,32 @@
-import { Line, XAxis, YAxis, ReferenceLine, ResponsiveContainer } from "recharts";
-import dynamic from "next/dynamic";
-import { IChannel } from "@/lib/interfaces/channels";
-import { setWaveformTimeFromChannel } from "@/lib/functions/setWaveformTimeFromChannel";
-import { getWaveformStats } from "@/lib/functions/getWaveformStats";
-import { useEEWS } from "@/lib/hooks/useEEWS";
-const LineChart = dynamic(() => import("recharts").then((recharts) => recharts.LineChart), { ssr: false });
+import {
+  Line,
+  XAxis,
+  YAxis,
+  ReferenceLine,
+  ResponsiveContainer,
+} from 'recharts';
+import dynamic from 'next/dynamic';
+import { IChannel } from '@/lib/interfaces/channels';
+import { setWaveformTimeFromChannel } from '@/lib/functions/setWaveformTimeFromChannel';
+import { getWaveformStats } from '@/lib/functions/getWaveformStats';
+import { useEEWS } from '@/lib/hooks/useEEWS';
+const LineChart = dynamic(
+  () => import('recharts').then((recharts) => recharts.LineChart),
+  { ssr: false }
+);
 
 export const ChannelChart: React.FC<{ channel: IChannel }> = ({ channel }) => {
-  const {packetsCount} = useEEWS()
-  if(channel.waveform.data.length == 0){
-    return <div></div>
+  const { packetsCount } = useEEWS();
+  if (channel.waveform.data.length == 0) {
+    return <div></div>;
   }
   const waveforms = setWaveformTimeFromChannel(channel, packetsCount);
   const [mean, max] = getWaveformStats(waveforms);
   const arr = channel.waveform.pick?.arrival;
   const nearest = arr ? waveforms.find((w) => w.time > arr) : null;
+
   return (
-    <div className="grid grid-cols-12" >
+    <div className="grid grid-cols-12">
       <div className="col-span-1 px-2 relative items-center gap-x-2 grid grid-cols-2">
         <div className="flex flex-col h-full justify-center">
           <h6>{channel.stationCode}</h6>
@@ -28,21 +38,21 @@ export const ChannelChart: React.FC<{ channel: IChannel }> = ({ channel }) => {
             <tbody>
               <tr>
                 <td>Amax</td>
-                <td>{`:${max ?? "-"}`}</td>
+                <td>{`:${max ?? '-'}`}</td>
               </tr>
               <tr>
                 <td>Mean</td>
-                <td>{`:${mean ?? "-"}`}</td>
+                <td>{`:${mean ?? '-'}`}</td>
               </tr>
               <tr>
                 <td>Arrival</td>
                 <td>
                   {`:${
                     nearest && arr
-                      ? new Date(arr).toLocaleTimeString("en-US", {
-                          hourCycle: "h24",
+                      ? new Date(arr).toLocaleTimeString('en-US', {
+                          hourCycle: 'h24',
                         })
-                      : "-"
+                      : '-'
                   }`}
                 </td>
               </tr>
@@ -52,37 +62,52 @@ export const ChannelChart: React.FC<{ channel: IChannel }> = ({ channel }) => {
       </div>
       <div className="col-span-11 w-full pt-4">
         <ResponsiveContainer width="100%" className="" height={100}>
-          <LineChart data={waveforms} margin={{ top: 5, left: 0, right: 0, bottom: 0 }}>
+          <LineChart
+            data={waveforms}
+            margin={{ top: 5, left: 0, right: 0, bottom: 0 }}
+          >
             <XAxis
               dataKey="time"
               xAxisId={0}
               axisLine={true}
               tickSize={10}
               interval={Math.floor(waveforms.length / 9) - 10}
-              domain={["auto", "auto"]}
-              padding={"gap"}
+              domain={['auto', 'auto']}
+              padding={'gap'}
               tickFormatter={(val) =>
                 val
-                  ? new Date(val).toLocaleTimeString("en-US", {
-                      hourCycle: "h24",
-                      hour: "2-digit",
-                      minute: "2-digit",
-                      second: "2-digit",
+                  ? new Date(val).toLocaleTimeString('en-US', {
+                      hourCycle: 'h24',
+                      hour: '2-digit',
+                      minute: '2-digit',
+                      second: '2-digit',
                     })
-                  : ""
+                  : ''
               }
             />
             <YAxis
               type="number"
               domain={([dataMin, dataMax]) => {
-                const absMax = Math.max(Math.abs(dataMin), Math.abs(dataMax), 5000);
+                const absMax = Math.max(
+                  Math.abs(dataMin),
+                  Math.abs(dataMax),
+                  5000
+                );
                 return [-absMax, absMax];
               }}
               ticks={[0]}
             />
-            <Line type="monotone" dataKey="value" stroke="#8884d8" strokeWidth={2} dot={false} />
+            <Line
+              type="monotone"
+              dataKey="value"
+              stroke="#8884d8"
+              strokeWidth={2}
+              dot={false}
+            />
             <ReferenceLine y={0} stroke="#888888" />
-            {nearest && <ReferenceLine stroke="red" strokeWidth={2} x={nearest.time} />}
+            {nearest && (
+              <ReferenceLine stroke="red" strokeWidth={2} x={nearest.time} />
+            )}
           </LineChart>
         </ResponsiveContainer>
       </div>
