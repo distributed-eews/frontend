55506a729c60042e4582b43868377e30f5a8be57
Taufik Pragusga <taufik@pragusga.com>
2023-12-06 15:47:40 +0700

    fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,19 @@
-import { EEWSProvider } from "@/lib/hooks/eewsCtx";
-import { Html, Head, Main, NextScript } from "next/document";
+import { EEWSProvider } from '@/lib/hooks/eewsCtx';
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en" className="bg-indigo-950">
-      <Head />
-        <body className="bg-slate-300">
-          <Main />
-          <NextScript />
-        </body>
-
+      <Head>
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </Head>
+      <body className="bg-slate-300">
+        <Main />
+        <NextScript />
+      </body>
     </Html>
   );
 }
