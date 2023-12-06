5d868ecc254cc6977abae2467af4a4fce977ff2d
zidan kharisma a <zidan.kharisma@ui.ac.id>
2023-12-07 01:29:33 +0700

    delete unused

--- lib/connection/pick.ts
+++
@@ -1,9 +0,0 @@
-import { IPacketWaveform } from "../interfaces/waveform";
-
-const onWaveformMessage = (setChannelPick: (stat: string, chan: string, time: string) => void) => {
-  return (ev: MessageEvent<any>) => {
-    const res = JSON.parse(ev.data);
-    var parsedPacket = res.value
-    setChannelPick(parsedPacket.station, parsedPacket.channel, parsedPacket.arrival);
-  };
-};
