68fc329887cdd5a73aab76b6c5246c6e71423e91
Taufik Pragusga <taufik@pragusga.com>
2024-02-09 13:28:36 +0700

    add docker

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,10 +1,10 @@
-import axios from "axios";
-import { BACKEND_URL } from "./credentials";
+import axios from 'axios';
+import { BACKEND_URL } from './credentials';
 
 export const AxiosClient = axios.create({
-  baseURL: "http://"+BACKEND_URL,
-  headers:{
-    "Accept": "*/*",
-    "Content-Type": "application/json",
-  }
+  baseURL: 'http://' + BACKEND_URL,
+  headers: {
+    Accept: '*/*',
+    'Content-Type': 'application/json',
+  },
 });
